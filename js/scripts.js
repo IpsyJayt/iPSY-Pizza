014b965b7eca050b7104a55a5d9da472ad5effed
IpsyJayt <ipsyjayt@gmail.com>
2021-11-29 01:09:26 +0300

    add jquery for slidetoggle on pizza menus

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+// USER INTERFACE LOGIC
+$(document).ready(function(){
+    $('.pizza-name1').click(function(){
+        $('.description1').slideToggle()
+    });
+    $('.pizza-name2').click(function(){
+        $('.description2').slideToggle()
+    });
+    $('.pizza-name3').click(function(){
+        $('.description3').slideToggle()
+    });
+    $('.pizza-name4').click(function(){
+        $('.description4').slideToggle()
+    });
+})
