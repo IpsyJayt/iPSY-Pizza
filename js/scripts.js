f3cdec5a90877ca0c15d3b68d8b327dba7516026
IpsyJayt <ipsyjayt@gmail.com>
2021-11-29 11:50:18 +0300

    add call function on submit

--- js/scripts.js
+++ js/scripts.js
@@ -13,10 +13,50 @@ $(document).ready(function(){
         $('.description4').slideToggle()
     });
 });
-// COLLECT DATA FROM USER
+
+
+
+function yourOrder(){
+    // COLLECT DATA FROM USER
 var pizzaName = document.getElementById('pizzaName').value;
 var pizzaSize = document.getElementById('sizeOfPizza').value;
 var pizzaTopping = document.getElementById('topping').value;
 var pizzaCrust = document.getElementById('crust').value;
-var pizzaNumber = document.getElementById('number');
+var pizzaNumber = parseInt(document.getElementById('number').value);
 var pizzaDelivery = document.getElementById('delivery').value;
+
+    var pizzaOrder = "Your order is " + pizzaNumber + " "+ pizzaSize +" "+ pizzaName
+    alert(pizzaOrder);
+
+    if(pizzaSize=="small"){
+        var sizePrice= 400
+    } else if (pizzaSize=="medium"){
+        sizePrice=500
+    }else if(pizzaSize=="large"){
+        sizePrice=700
+    };
+    if (pizzaTopping == "none"){
+        var toppingPrize = 0
+    } else if (pizzaTopping=="Pineapple toppings"){
+        toppingPrize=150
+    }else if (pizzaTopping=="Onions toppings"){
+        toppingPrize = 100
+    } else if (pizzaTopping == "Bacon toppings"){
+        toppingPrize = 200
+    };
+    if(pizzaCrust=="Gluten free"){
+        var crustPrize= 150
+    } else if(pizzaCrust=="Crispy"){
+        crustPrize=200
+    } else if(pizzaCrust=="Stuffed"){
+        crustPrize == 250
+    };
+    var total = (sizePrice + toppingPrize + crustPrize)*pizzaNumber
+    if(pizzaDelivery=="yes"){
+        var deliveryLocation = prompt("Enter Delivery Location")
+        alert("Your order of " +pizzaNumber+" "+pizzaCrust+" "+ pizzaName +" "+" with " + pizzaTopping + " costs " + total+ " and will be delivered to " + deliveryLocation)
+    }else {
+        alert ("Pick your " + pizzaNumber+ " "+pizzaCrust+" "+pizzaName+ " with " +pizzaTopping+ " at iPSY Pizza")
+    }
+
+}
